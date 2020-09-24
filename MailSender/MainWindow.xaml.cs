78f9fe28a87861aabbceae3290bdce20671154d2
Zera57 <svetik.avtonomov@gmail.com>
2020-09-24 15:37:49 +0300

    Закончил основное приложение

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Mail;
+using System.Security;
 
 namespace MailSender
 {
@@ -23,6 +26,70 @@ namespace MailSender
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+		private void SendButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (TBLogin.Text.Length != 0 && TBPassword.Password.Length != 0 && TBEmailto.Text.Length != 0 && TBSubject.Text.Length != 0 && TBMessage.Text.Length != 0)
+			{
+				EmailSendServiceClass senderService = new EmailSendServiceClass(Data.host, Data.port);
+
+				senderService.InitMessage(TBSubject.Text, TBMessage.Text, new MailAddress(TBLogin.Text), new MailAddress(TBEmailto.Text));
+
+				senderService.Creditionals(TBLogin.Text, TBPassword.SecurePassword);
+
+				senderService.SendMessage();
+			}
+			else { MessageBox.Show("Type all data"); }
+		}
+
+		public static class Data
+		{
+			public static string host = "smtp.gmail.com";
+			public static int port = 587;
+
+
+		}
+	}
+
+	public class EmailSendServiceClass
+	{
+		SmtpClient client;
+		MailMessage message;
+
+
+		public EmailSendServiceClass(string host, int port)
+		{
+			client = new SmtpClient(host, port);
+			client.EnableSsl = true;
+		}
+
+		public void InitMessage(string subject, string body, MailAddress from, MailAddress to)
+		{
+			message = new MailMessage(from, to);
+			message.Subject = subject;
+			message.Body = body;
+		}
+
+		public void Creditionals(string login, SecureString password)
+		{
+			client.Credentials = new NetworkCredential
+			{
+				UserName = login,
+				SecurePassword = password
+			};
+		}
+
+		public void SendMessage()
+		{
+			try
+			{
+				client.Send(message);
+				MessageBox.Show("Mail sended");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
 		}
 	}
 }
