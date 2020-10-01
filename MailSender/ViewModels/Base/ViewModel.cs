a1a91560ff7ead5f6f4fa9e005ba2d6aafef0786
Zera57 <svetik.avtonomov@gmail.com>
2020-10-01 16:27:10 +0300

    Revert "Revert "Создал ViewModel""
    This reverts commit 516fda580eff4ca1b5eee758e2d675ae3f259130.

---
+++ MailSender/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MailSender.ViewModels
+{
+	class ViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string PropertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+		}
+	}
+}
