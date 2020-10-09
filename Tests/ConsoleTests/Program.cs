9c573cff3dbfd64a134d45ccbc6ed1fed2c24275
Zera57 <svetik.avtonomov@gmail.com>
2020-10-09 13:27:29 +0300

    Сделал такой же ужасный сумматор(

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -23,8 +23,12 @@ namespace ConsoleTests
 			factorial(Convert.ToInt32(Console.ReadLine()));
 		}
 
-		static long result = 1;
-		static bool done = false;
+		static long f_result = 1;
+		static bool f_done = false;
+
+		static long s_result = 0;
+		static bool s_done = false;
+
 
 		static void factorial(int n)
 		{
@@ -32,32 +36,62 @@ namespace ConsoleTests
 			func1.Start();
 			var func2 = new Thread(() => f2(n));
 			func2.Start();
+			var sum1 = new Thread(() => s1(n));
+			sum1.Start();
+			var sum2 = new Thread(() => s2(n));
+			sum2.Start();
 		}
 
 		static void f1(int n)
 		{
 			for (int i = 1; i <= n/2; i++)
 			{
-				result *= i;
+				f_result *= i;
 			}
-			Print_Result(n);
+			Print_F_Result(n);
 		}
 
 		static void f2(int n)
 		{
 			for (int i = n/2+1; i <= n; i++)
 			{
-				result *= i;
+				f_result *= i;
 			}
-			Print_Result(n);
+			Print_F_Result(n);
 		}
 
-		static void Print_Result(int n)
+		static void s1(int n)
 		{
-			if (done == false)
-				done = true;
+			for (int i = 1; i <= n / 2; i++)
+			{
+				s_result += i;
+			}
+			Print_S_Result(n);
+		}
+
+		static void s2(int n)
+		{
+			for (int i = n / 2 + 1; i < n; i++)
+			{
+				s_result += i;
+			}
+			Print_S_Result(n);
+		}
+
+		static void Print_F_Result(int n)
+		{
+			if (f_done == false)
+				f_done = true;
 			else
-				Console.WriteLine($"Facrotial for {n} is {result}");
+				Console.WriteLine($"Facrotial for {n} is {f_result}");
+		}
+
+		static void Print_S_Result(int n)
+		{
+			if (s_done == false)
+				s_done = true;
+			else
+				Console.WriteLine($"Sum of all numbers bellow {n} is {s_result}");
 		}
 
 		public static void ThreadInfo()
