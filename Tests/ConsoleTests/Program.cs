f36959437801e634459ea8325004adf4d01ed828
Zera57 <svetik.avtonomov@gmail.com>
2020-10-13 01:04:43 +0300

    Написал программу по умножению матриц с использованием параллельным умножением.

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -2,102 +2,82 @@
 using System.Net;
 using System.Net.Mail;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ConsoleTests
 {
 	class Program
 	{
+
+		static int[,] a = new int[3, 3]
+		{
+			{ 1, 2, 3 },
+			{ 4, 5, 6 },
+			{ 7, 8, 9 }
+		};
+
+
+		static int[,] b = new int[3,3]
+		{
+			{ 1, 2, 3 },
+			{ 4, 5, 6 },
+			{ 7, 8, 9 }
+		};
+
 		static void Main(string[] args)
 		{
-			#region
-			//var Timer_Thread = new Thread(Timer);
+			
+			var task = Matrix_Multiply_Async.StartAsync(a, b);
 
-			//Timer_Thread.Name = "timer";
+			task.Wait();
 
-			//Timer_Thread.Start();
-
-			//Console.WriteLine("Main thread is done.");
-			//ThreadInfo();
-			#endregion
-			Console.WriteLine("Type number");
-			factorial(Convert.ToInt32(Console.ReadLine()));
+			Console.WriteLine("Задача выполнена");
 		}
+	}
 
-		static long f_result = 1;
-		static bool f_done = false;
-
-		static long s_result = 0;
-		static bool s_done = false;
-
-
-		static void factorial(int n)
+	static class Matrix_Multiply_Async
+	{
+		static void Print_Matrix(int[,] result)
 		{
-			var func1 = new Thread(() => f1(n));
-			func1.Start();
-			var func2 = new Thread(() => f2(n));
-			func2.Start();
-			var sum1 = new Thread(() => s1(n));
-			sum1.Start();
-			var sum2 = new Thread(() => s2(n));
-			sum2.Start();
-		}
-
-		static void f1(int n)
-		{
-			for (int i = 1; i <= n/2; i++)
+			for (int i = 0; i < 3; i++)
 			{
-				f_result *= i;
+				for (int j = 0; j < 3; j++)
+				{
+					Console.Write(result[i, j] + "\t");
+				}
+				Console.WriteLine();
 			}
-			Print_F_Result(n);
 		}
 
-		static void f2(int n)
+		static public async Task StartAsync(int[,] a, int[,] b)
 		{
-			for (int i = n/2+1; i <= n; i++)
+			var c = new int[3,3];
+
+
+			for (int i = 0; i < 3; i++)
 			{
-				f_result *= i;
+				for (int j = 0; j < 3; j++)
+				{
+					var result = await GetResultAsync(a, b, i, j);
+
+					c[i, j] = result;
+				}
 			}
-			Print_F_Result(n);
+			Print_Matrix(c);
 		}
 
-		static void s1(int n)
+		public static Task<int> GetResultAsync(int[,] a, int[,] b, int i, int j)
 		{
-			for (int i = 1; i <= n / 2; i++)
+			return Task.Run(() =>
 			{
-				s_result += i;
-			}
-			Print_S_Result(n);
+				int result = 0;
+				for (int z = 0; z < 3; z++)
+				{
+					result += a[i, z] * b[z, j];
+				}
+				return result;
+			});
 		}
 
-		static void s2(int n)
-		{
-			for (int i = n / 2 + 1; i < n; i++)
-			{
-				s_result += i;
-			}
-			Print_S_Result(n);
-		}
-
-		static void Print_F_Result(int n)
-		{
-			if (f_done == false)
-				f_done = true;
-			else
-				Console.WriteLine($"Facrotial for {n} is {f_result}");
-		}
-
-		static void Print_S_Result(int n)
-		{
-			if (s_done == false)
-				s_done = true;
-			else
-				Console.WriteLine($"Sum of all numbers bellow {n} is {s_result}");
-		}
-
-		public static void ThreadInfo()
-		{
-			var thread = Thread.CurrentThread;
-			Console.WriteLine($"id:\t\t{thread.ManagedThreadId}; \nname:\t\t{thread.Name}; \npriority:\t{thread.Priority}\n");
-		}
 	}
 }
