13493ae66a6b36da20316a03586f15bbbfa52b17
Zera57 <svetik.avtonomov@gmail.com>
2020-10-16 21:47:20 +0300

    донастройка

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using MailSender.lib.Interfaces;
+﻿using MailSender.Data;
+using MailSender.lib.Interfaces;
 using MailSender.lib.Service;
 using MailSender.ViewModels;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Windows;
 
 namespace MailSender
 {
@@ -25,6 +28,14 @@ namespace MailSender
 			services.AddSingleton<MainWindowViewModel>();
 			services.AddSingleton<IEncryptorService, Rfc2898Encryptor>();
 			services.AddTransient<IMailService, SmtpMailService>();
+			services.AddDbContext<MailSenderDB>(opt => opt.UseSqlServer(host.Configuration.GetConnectionString("Default")));
+			services.AddTransient<MailSenderDbInitializer>();
+		}
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			Services.GetRequiredService<MailSenderDbInitializer>().Initialize();
+			base.OnStartup(e);
 		}
 	}
 }
---
+++ MailSender/Data/MailSenderDbInitializer.cs
@@ -0,0 +1,58 @@
+﻿using MailSender.Data.Migrations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.Data
+{
+	class MailSenderDbInitializer
+	{
+		private readonly MailSenderDB _DB;
+
+		public MailSenderDbInitializer(MailSenderDB DB) => _DB = DB;
+
+		public void Initialize()
+		{
+			_DB.Database.Migrate();
+
+			InitializeRecipients();
+			InitializeSenders();
+			InitializeServers();
+			InitializeMessages();
+		}
+
+		private void InitializeRecipients()
+		{
+			if (_DB.Recipients.Any()) return;
+
+			_DB.Recipients.AddRange(TestData.Recipients);
+			_DB.SaveChanges();
+		}
+
+		private void InitializeSenders()
+		{
+			if (_DB.Senders.Any()) return;
+
+			_DB.Senders.AddRange(TestData.Senders);
+			_DB.SaveChanges();
+		}
+
+		private void InitializeServers()
+		{
+			if (_DB.Servers.Any()) return;
+
+			_DB.Servers.AddRange(TestData.Servers);
+			_DB.SaveChanges();
+		}
+
+		private void InitializeMessages()
+		{
+			if (_DB.Messages.Any()) return;
+
+			_DB.Messages.AddRange(TestData.Messages);
+			_DB.SaveChanges();
+		}
+	}
+}
