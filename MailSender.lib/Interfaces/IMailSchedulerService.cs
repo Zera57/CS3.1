9eb2daeb5bab51fca3a4f2dcb217ea4f21786489
Zera57 <svetik.avtonomov@gmail.com>
2020-10-19 11:55:05 +0300

    SchedulerService

---
+++ MailSender.lib/Interfaces/IMailSchedulerService.cs
@@ -0,0 +1,16 @@
+﻿using MailSender.lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.lib.Interfaces
+{
+	interface IMailSchedulerService
+	{
+		void Start();
+
+		void Stop();
+
+		void AddTask(DateTime Time, Sender sender, IEnumerable<Recipient>, Server Server, Message Message);
+	}
+}
