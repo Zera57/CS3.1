422b2dc503dafd185db54ab45afbbd633d37c770
Zera57 <svetik.avtonomov@gmail.com>
2020-10-19 11:45:52 +0300

    Внедрение асинхронных методов

--- MailSender.lib/Service/SmtpMailService.cs
+++ MailSender.lib/Service/SmtpMailService.cs
@@ -2,9 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MailSender.lib.Service
 {
@@ -64,6 +67,72 @@ namespace MailSender.lib.Service
 					}
 				}
 			}
+		}
+
+		public void Send(string SenderAddress, IEnumerable<string> RecipientAddress, string Subject, string Body)
+		{
+			foreach (var recipient_address in RecipientAddress)
+				Send(SenderAddress, recipient_address, Subject, Body);
+		}
+
+		public async Task SendAsync(string SenderAddress, string RecipientAddress, string Subject, string Body, CancellationToken Cancel = default)
+		{
+			var from = new MailAddress(SenderAddress);
+			var to = new MailAddress(RecipientAddress);
+
+			using (var message = new MailMessage(from, to))
+			{
+				message.Subject = Subject;
+				message.Body = Body;
+
+				using (var client = new SmtpClient(_Address, _Port))
+				{
+					client.EnableSsl = _SSL;
+
+
+					client.Credentials = new NetworkCredential
+					{
+						UserName = _Login,
+						Password = _Password
+					};
+					try
+					{
+						Cancel.ThrowIfCancellationRequested();
+
+						await client.SendMailAsync(message).ConfigureAwait(false);
+					}
+					catch (SmtpException e)
+					{
+						Trace.TraceError(e.ToString());
+						throw;
+					}
+				}
+			}
+		}
+
+		public async Task SendAsync(
+			string SenderAddress, IEnumerable<string> RecipientAddress, string Subject, string Body,
+			CancellationToken Cancel = default)
+		{
+			var recipients = RecipientAddress.ToArray();
+
+			for (int i = 0, count = recipients.Length; i < count; i++)
+			{
+				Cancel.ThrowIfCancellationRequested();
+
+				await SendAsync(SenderAddress, recipients[i], Subject, Body).ConfigureAwait(false);
+
+				//Progress?.Report((recipients[i], i / (double)count));
+			}
+		}
+
+		public async Task SendParallelAsync(
+			string SenderAddress, IEnumerable<string> RecipientAddress, string Subject, string Body,
+			CancellationToken Cancel = default)
+		{
+			var tasks = RecipientAddress.Select(recipient_addresess => SendAsync(SenderAddress, recipient_addresess, Subject, Body, Cancel));
+
+			await Task.WhenAll(tasks).ConfigureAwait(false);
 		}
 	}
 }
