38259234d8506c14af2078916795ac610189f354
Zera57 <svetik.avtonomov@gmail.com>
2020-10-19 10:04:27 +0300

    Интерфейс для работы с хранилищем

---
+++ MailSender.lib/Interfaces/IStore.cs
@@ -0,0 +1,20 @@
+﻿using MailSender.lib.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.lib.Interfaces
+{
+	public interface IStore<T> where T : Entity
+	{
+		IEnumerable<T> GetAll();
+
+		T GetById(int Id);
+
+		T Add(T Item);
+
+		void Update(T Item);
+
+		void Delete(int Id);
+	}
+}
