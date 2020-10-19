563efe4c35e843e5b8e0cc2c004be71aaec04244
Zera57 <svetik.avtonomov@gmail.com>
2020-10-19 10:06:08 +0300

    Сервисы для работы с данными в бд

---
+++ MailSender/Data/Stores/InDB/EntitiesStoreInDB.cs
@@ -0,0 +1,29 @@
+﻿using MailSender.lib.Models;
+
+namespace MailSender.Data.Stores.InDB
+{
+	class RecipientsStoreInDB : StoreInDB<Recipient>
+	{
+		public RecipientsStoreInDB(MailSenderDB db) : base(db) { }
+	}
+
+	class SendersStoreInDB : StoreInDB<Sender>
+	{
+		public SendersStoreInDB(MailSenderDB db) : base(db) { }
+	}
+
+	class ServersStoreInDB : StoreInDB<Server>
+	{
+		public ServersStoreInDB(MailSenderDB db) : base(db) { }
+	}
+
+	class MessagesStoreInDB : StoreInDB<Message>
+	{
+		public MessagesStoreInDB(MailSenderDB db) : base(db) { }
+	}
+
+	class SchedulerTasksStoreInDB : StoreInDB<SchedulerTask>
+	{
+		public SchedulerTasksStoreInDB(MailSenderDB db) : base(db) { }
+	}
+}
